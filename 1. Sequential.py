214e84c4e063c8aeeb643147044885160206d91a
Taufik Budi Santosa <taufikbudis@gmail.com>
2023-04-02 08:45:09 +0800

    Sequential

--- 1. Sequential.py
+++ 1. Sequential.py
@@ -1,1 +1,7 @@
 # 1. Sequential : Successive steps.
+
+print('Mother said, "Go to the store!"')
+print('The child replies, "Okay, what should I do at the store?"')
+print('Mother replied, "Buy one bottle of milk and if there are eggs buy 6 eggs')
+print('So the child went to the shop')
+print('And start shopping')
