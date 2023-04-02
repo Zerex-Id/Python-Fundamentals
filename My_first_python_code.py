7aff9a822743db44fef4284477edca80dfb431e6
Taufik Budi Santosa <taufikbudis@gmail.com>
2023-04-02 08:14:56 +0800

    My first Python code

---
+++ My_first_python_code.py
@@ -0,0 +1,10 @@
+# The sign (#) is a comment that will not be executed (for one line)
+
+"""
+Writing a comment of more than one line is using double quotes 3x
+and closed with double quotes 3x as well.
+"""
+
+print("Hello World")
+print("My name is Taufik")
+print("I'm learning Python with PyCharm")
